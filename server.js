aabebe2aaced012f6b836da5b24182ebeb19e21f
Illia Husak <illia.husak@it.bekey.dk>
2021-01-27 16:37:28 +0200

    Refactoring gmail access. Cross origin prod client

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if(!process.env.MONGODB_URI) {
 } else {
   app.use(function(req, res, next) {
     console.log('cross origin prod');
-    res.header("Access-Control-Allow-Origin", 'https://librarytricks.herokuapp.com');
+    res.header("Access-Control-Allow-Origin", 'http://lb.afreestylers.com');
     res.header("Access-Control-Allow-Credentials", 'true');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json, Authorization');
