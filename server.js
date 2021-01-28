49e9de640359f86b68a15ccac51bfdd55535d159
Illia Husak <ilyagusak@gmail.com>
2021-01-28 22:21:14 +0200

    change cors to origin, test req

--- server.js
+++ server.js
@@ -18,21 +18,21 @@ if(!process.env.MONGODB_URI) {
 }
   app.use(cors(corsOptions))
 } else {
-  // var corsProdOptions = {
-  //   origin: `http://lb.afreestylers.com`,
-  //   credentials: true,
-  //   optionsSuccessStatus: 200,
-  //   methods: 'GET,PUT,POST,DELETE,OPTIONS'
-  // };
-  // app.use(cors(corsProdOptions));
-  app.use(function(req, res, next) {
-    console.log('cross origin prod');
-    res.header("Access-Control-Allow-Origin", 'http://lb.afreestylers.com');
-    res.header("Access-Control-Allow-Credentials", 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json, Authorization');
-    next();
-});
+  var corsProdOptions = {
+    origin: `http://lb.afreestylers.com`,
+    credentials: true,
+    optionsSuccessStatus: 200,
+    methods: 'GET,PUT,POST,DELETE,OPTIONS'
+  };
+  app.use(cors(corsProdOptions));
+//   app.use(function(req, res, next) {
+//     console.log('cross origin prod');
+//     res.header("Access-Control-Allow-Origin", 'http://lb.afreestylers.com');
+//     res.header("Access-Control-Allow-Credentials", 'true');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json, Authorization');
+//     next();
+// });
 }
 console.log('DB_URI', DB_URL);
 console.log('PROD env', process.env);
