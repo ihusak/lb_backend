8864fa6e53cbc487979898e2b7d659af5be25299
Illia Husak <ilyagusak@gmail.com>
2021-03-22 00:25:01 +0200

    LBAPI-19. Fix email sending

--- backend/config/email.js
+++ backend/config/email.js
@@ -6,10 +6,10 @@ const myOAuth2Client = new OAuth2(
   "xf9ijHAX_ZBupYsyCxiFiKNs",
   "https://developers.google.com/oauthplayground"
 );
-myOAuth2Client.setCredentials({
-  refresh_token: '1//04WpydTp6lIH1CgYIARAAGAQSNwF-L9IrHr8gbQDsa1Cg5gtt8eMIvBARRLq-pwMEdJrGxE3OJFiEjMDNcYbl357otnkx1f2Z-PU'
-});
-const myAccessToken = myOAuth2Client.getAccessToken()
+// myOAuth2Client.setCredentials({
+//   refresh_token: '1//04-K4l5Z08NkfCgYIARAAGAQSNwF-L9IrPNaGVKJO33bPXuv4P2C5PWnRfh-a5uWSetukPrGylk4CA-GxfFhNRQDZ_dB1uOrv_c8'
+// });
+// const myAccessToken = myOAuth2Client.getAccessToken();
 
 exports.transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
@@ -20,7 +20,12 @@ exports.transporter = nodemailer.createTransport({
     user: 'afreestylers2016@gmail.com',
     clientId: '743800966740-v90gbc6ltd3hm56ndh5ni32hc3l0uqp7.apps.googleusercontent.com',
     clientSecret: 'xf9ijHAX_ZBupYsyCxiFiKNs',
-    refreshToken: '1//04WpydTp6lIH1CgYIARAAGAQSNwF-L9IrHr8gbQDsa1Cg5gtt8eMIvBARRLq-pwMEdJrGxE3OJFiEjMDNcYbl357otnkx1f2Z-PU',
-    accessToken: myAccessToken
+    refreshToken: '1//04-K4l5Z08NkfCgYIARAAGAQSNwF-L9IrPNaGVKJO33bPXuv4P2C5PWnRfh-a5uWSetukPrGylk4CA-GxfFhNRQDZ_dB1uOrv_c8',
+    accessToken: (() => {
+      myOAuth2Client.setCredentials({
+        refresh_token: '1//04-K4l5Z08NkfCgYIARAAGAQSNwF-L9IrPNaGVKJO33bPXuv4P2C5PWnRfh-a5uWSetukPrGylk4CA-GxfFhNRQDZ_dB1uOrv_c8'
+      });
+      return myOAuth2Client.getAccessToken();
+    })()
   }
 });
