212e712904527ab4c650ce9c2d2a37d26574b843
Illia Husak <ilyagusak@gmail.com>
2021-06-02 01:26:21 +0300

    LBAPI-25. Google api problem

--- backend/config/email.js
+++ backend/config/email.js
@@ -2,16 +2,34 @@ const nodemailer = require('nodemailer');
 const { google } = require("googleapis");
 const {mailTransporterLogger} = require('../config/middleware/logger');
 const OAuth2 = google.auth.OAuth2;
+const fetch = require("node-fetch");
+const code = '4/0AY0e-g5CMy8Wb34IkdcyAsD6O9Jf1ORMiyr1tl9MAEFgk02HKKpf3bC6NuhsUrvXt14JPg';
+const REFRESH_TOKEN = '1//04vbBUFVhnSDKCgYIARAAGAQSNwF-L9IrgZKxArVXASq1ZN_4o057B9_gKGaam6wd7X5mcOH2VTZzKvQEphsylodHMErsNN-mD9M';
+// const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
 
 exports.createTransporter = async () => {
   const myOAuth2Client = new OAuth2(
-    "743800966740-v90gbc6ltd3hm56ndh5ni32hc3l0uqp7.apps.googleusercontent.com",
-    "xf9ijHAX_ZBupYsyCxiFiKNs",
-    "https://developers.google.com/oauthplayground",
-    {forceRefreshOnFailure: true}
+  {
+    clientId: "743800966740-v90gbc6ltd3hm56ndh5ni32hc3l0uqp7.apps.googleusercontent.com",
+    clientSecret: "xf9ijHAX_ZBupYsyCxiFiKNs",
+    redirectUri: "https://developers.google.com/oauthplayground",
+    forceRefreshOnFailure: true
+  }
   );
+  try {
+    const response = await myOAuth2Client.getToken({
+      code: code,
+      client_id: "743800966740-v90gbc6ltd3hm56ndh5ni32hc3l0uqp7.apps.googleusercontent.com",
+      redirect_uri: "https://developers.google.com/oauthplayground",
+      codeVerifier: 'authorization_code'
+    });
+    console.log('response', response);
+  } catch (error) {
+    console.log('error', error);
+  }
   myOAuth2Client.setCredentials({
-    refresh_token: '1//0485DcXjSoChpCgYIARAAGAQSNwF-L9IrshC07kdLjWoBkp2Mt_BQ2_TH0Ow5ybV29dGckycYox7v7lB18XxI6TW-k93LQL5jI3Y'
+    refresh_token: REFRESH_TOKEN
   });
   const accessToken = await new Promise((resolve, reject) => {
     myOAuth2Client.getAccessToken((err, token) => {
@@ -33,9 +51,8 @@ exports.createTransporter = async () => {
       user: 'afreestylers2016@gmail.com',
       clientId: '743800966740-v90gbc6ltd3hm56ndh5ni32hc3l0uqp7.apps.googleusercontent.com',
       clientSecret: 'xf9ijHAX_ZBupYsyCxiFiKNs',
-      refreshToken: '1//0485DcXjSoChpCgYIARAAGAQSNwF-L9IrshC07kdLjWoBkp2Mt_BQ2_TH0Ow5ybV29dGckycYox7v7lB18XxI6TW-k93LQL5jI3Y',
-      accessToken,
-      expires: 1717987187825
+      refreshToken: REFRESH_TOKEN,
+      accessToken
     }
   });
   return transporter;
