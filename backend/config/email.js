d8e95b0e59202764eb8e5916c321a592a6c60ddb
Illia Husak <illia.husak@it.bekey.dk>
2021-06-15 10:36:22 +0300

    LBAPI-25. change git ignore

--- backend/config/email.js
+++ backend/config/email.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer');
 const { google } = require("googleapis");
+const config = require("../../config.json");
 const {mailTransporterLogger} = require('../config/middleware/logger');
 const OAuth2 = google.auth.OAuth2;
 const fetch = require("node-fetch");
@@ -9,50 +10,59 @@ const REFRESH_TOKEN = '1//04vbBUFVhnSDKCgYIARAAGAQSNwF-L9IrgZKxArVXASq1ZN_4o057B
 const REDIRECT_URI = "https://developers.google.com/oauthplayground";
 
 exports.createTransporter = async () => {
-  const myOAuth2Client = new OAuth2(
-  {
-    clientId: "743800966740-v90gbc6ltd3hm56ndh5ni32hc3l0uqp7.apps.googleusercontent.com",
-    clientSecret: "xf9ijHAX_ZBupYsyCxiFiKNs",
-    redirectUri: "https://developers.google.com/oauthplayground",
-    forceRefreshOnFailure: true
-  }
-  );
-  try {
-    const response = await myOAuth2Client.getToken({
-      code: code,
-      client_id: "743800966740-v90gbc6ltd3hm56ndh5ni32hc3l0uqp7.apps.googleusercontent.com",
-      redirect_uri: "https://developers.google.com/oauthplayground",
-      codeVerifier: 'authorization_code'
-    });
-    console.log('response', response);
-  } catch (error) {
-    console.log('error', error);
-  }
-  myOAuth2Client.setCredentials({
-    refresh_token: REFRESH_TOKEN
-  });
-  const accessToken = await new Promise((resolve, reject) => {
-    myOAuth2Client.getAccessToken((err, token) => {
-      if (err) {
-        mailTransporterLogger.info('GetAccessTokenErr', err);
-        reject("Failed to create access token :(");
-      }
-      resolve(token);
-    });
-  }).catch((err) => {
-    mailTransporterLogger.info('GetAccessTokenErr catch', err);
-  });
+  // const myOAuth2Client = new OAuth2(
+  // {
+  //   clientId: "743800966740-v90gbc6ltd3hm56ndh5ni32hc3l0uqp7.apps.googleusercontent.com",
+  //   clientSecret: "xf9ijHAX_ZBupYsyCxiFiKNs",
+  //   redirectUri: "https://developers.google.com/oauthplayground",
+  //   forceRefreshOnFailure: true
+  // }
+  // );
+  // try {
+  //   const response = await myOAuth2Client.getToken({
+  //     code: code,
+  //     client_id: "743800966740-v90gbc6ltd3hm56ndh5ni32hc3l0uqp7.apps.googleusercontent.com",
+  //     redirect_uri: "https://developers.google.com/oauthplayground",
+  //     codeVerifier: 'authorization_code'
+  //   });
+  //   console.log('response', response);
+  // } catch (error) {
+  //   console.log('error', error);
+  // }
+  // myOAuth2Client.setCredentials({
+  //   refresh_token: REFRESH_TOKEN
+  // });
+  // const accessToken = await new Promise((resolve, reject) => {
+  //   myOAuth2Client.getAccessToken((err, token) => {
+  //     if (err) {
+  //       mailTransporterLogger.info('GetAccessTokenErr', err);
+  //       reject("Failed to create access token :(");
+  //     }
+  //     resolve(token);
+  //   });
+  // }).catch((err) => {
+  //   mailTransporterLogger.info('GetAccessTokenErr catch', err);
+  // });
+  // const transporter = nodemailer.createTransport({
+  //   host: 'smtp.gmail.com',
+  //   port: 465,
+  //   secure: true,
+  //   auth: {
+  //     type: 'OAuth2',
+  //     user: 'afreestylers2016@gmail.com',
+  //     clientId: '743800966740-v90gbc6ltd3hm56ndh5ni32hc3l0uqp7.apps.googleusercontent.com',
+  //     clientSecret: 'xf9ijHAX_ZBupYsyCxiFiKNs',
+  //     refreshToken: REFRESH_TOKEN,
+  //     accessToken
+  //   }
+  // });
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    // port: 465,
+    // secure: true,
     auth: {
-      type: 'OAuth2',
-      user: 'afreestylers2016@gmail.com',
-      clientId: '743800966740-v90gbc6ltd3hm56ndh5ni32hc3l0uqp7.apps.googleusercontent.com',
-      clientSecret: 'xf9ijHAX_ZBupYsyCxiFiKNs',
-      refreshToken: REFRESH_TOKEN,
-      accessToken
+      user: config.mail.user,
+      pass: config.mail.pass
     }
   });
   return transporter;
